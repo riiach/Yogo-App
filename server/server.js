97a9f76e3840c63e85481373555ddbb4cc08cb56
Ria Choi <riiachoii@gmail.com>
2026-02-02 21:29:14 +0900

    server.js typo fixed

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const client = new vision.ImageAnnotatorClient({
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     },
-    projectID: process.env.GOOGLE_PROJECT_ID,
+    projectId: process.env.GOOGLE_PROJECT_ID,
 });
 
 app.use(express.json()); // <-- IMPORTANT
